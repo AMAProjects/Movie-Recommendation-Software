a00d43fef9cae8bf4175f989e91ec5450851a82c
AMAProjects <angelamolinaalhambra@gmail.com>
2025-02-15 15:35:06 +0000

    Created a filters() outside of the rest for better functionality

--- script.py
+++ script.py
@@ -16,20 +16,13 @@ def insert_genres():
         print('- ' + str(i) + '. ' + genre + '\n')
 
     chosen_genre = input('Please select the number corresponding to the genre you want to look for: ')
-    display_movies_from = genres[int(chosen_genre)]
+    display_movies_from_genre = genres[int(chosen_genre)]
 
     #Ask if user wants to apply any filters to their seach
-    apply_filters = input(f"\nBefore we display the movies under {display_movies_from} genre; Would you like to apply any filters to your search? \n\n Filters available: \n - Age rating \n - Movie rating \n\n Please indicate 'y' for yes or 'n' no:  ")
+    filters(display_movies_from_genre)
     
-    if apply_filters.lower() == 'y':
-        pass #TO DO!
-    if apply_filters.lower() == 'n':
-        pass #TO DO
-    else:
-        print('Please enter a valid input')
-    
-    print(f'\nThe movies in the {display_movies_from} genre are:\n' )
-    for movie in movies[display_movies_from]:
+    print(f'\nThe movies in the {display_movies_from_genre} genre are:\n' )
+    for movie in movies[display_movies_from_genre]:
         print(f'\n - {movie['title']}, \n   rating: {movie['rating']}  \n   Suitable for ages: {movie['age_recommendation']}')
 
 #Function to ask about search type
@@ -39,6 +32,17 @@ def start_search():
         print(insert_genres())
     if search_type == 'n':
         neame_search = input ('Please enter the name of first letter/s of the movie you are looking for: ')
+
+#Apply filters function
+def filters(filtered_genre):
+    apply_filters = input(f"\nBefore we display the movies under {filtered_genre} genre; Would you like to apply any filters to your search? \n\n Filters available: \n - Age rating \n - Movie rating \n\n Please indicate 'y' for yes or 'n' no:  ")
+    
+    if apply_filters.lower() == 'y':
+        pass #TO DO!
+    if apply_filters.lower() == 'n':
+        pass #TO DO
+    else:
+        print('Please enter a valid input')
 
 
 call_program()

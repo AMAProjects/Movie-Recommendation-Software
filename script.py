f899059bf0d5b2afc7138f78fc476e7da68a113f
AMAProjects <angelamolinaalhambra@gmail.com>
2025-02-23 21:03:37 +0000

    Added again functionality to exit the program

--- script.py
+++ script.py
@@ -15,7 +15,7 @@ def insert_genres():
     for i, genre in enumerate(genres, start = 0):
         print('- ' + str(i) + '. ' + genre + '\n')
 
-    chosen_genre = input('Please select the number corresponding to the genre you want to look for: ')
+    chosen_genre = input('Please select the number corresponding to the genre you want to look for: ') 
     display_movies_from_genre = genres[int(chosen_genre)]
 
     #Ask if user wants to apply any filters to their seach
@@ -25,6 +25,15 @@ def insert_genres():
     for movie in movies[display_movies_from_genre]:
         print(f'\n - {movie['title']}, \n   rating: {movie['rating']}  \n   Suitable for ages: {movie['age_recommendation']}')
     
+     # After this point, you can either exit or loop back to another function, but not start_search() again
+    again = input("\nWould you like to search again? (y/n)").lower()
+
+    if again == 'y':
+        start_search()  # Call again if the user wants to start a new search  
+    else:
+        print("\nThank you for using the movie search program!")
+        exit()
+
    
 
 
@@ -32,10 +41,14 @@ def insert_genres():
 def start_search():
     search_type = str(input("Would you like to search by name or genre? Write 'n' for 'name' and 'g' for 'genre': "))
     if search_type.lower() == 'g':
-        print(insert_genres())
-    if search_type.lower() == 'n':
-        neame_search = input ('Please enter the name of first letter/s of the movie you are looking for: ')
+        insert_genres()
+    elif search_type.lower() == 'n':
+        print("Name search functionality not implemented yet.")
+        #name_search = input ('\nPlease enter the name of first letter/s of the movie you are looking for: \n')
         #TO DO
+    else:
+        print("\nPlease select either 'g' or 'n'\n")
+        start_search()
 
 #Apply filters function
 def filters(filtered_item):
@@ -122,10 +135,11 @@ def filters(filtered_item):
             print("Invalid input. Please enter 'a', 'r', or 'b'.")
             filters(filtered_item)  # Recursively call to retry
 
-        if apply_filters.lower() == 'n':
-            pass #TO DO
-        else:
-            print('Please enter a valid input')
+    if apply_filters.lower() == 'n':
+        return
+    
+    else:
+        print('Please enter a valid input')
 
 
 call_program()

0b16374b4faa7ce0b7dae14a7c9251cd72367104
AMAProjects <angelamolinaalhambra@gmail.com>
2025-02-23 20:22:04 +0000

    Small fixes

--- script.py
+++ script.py
@@ -20,18 +20,22 @@ def insert_genres():
 
     #Ask if user wants to apply any filters to their seach
     filters(display_movies_from_genre)
-    
+        
     print(f'\nThe movies in the {display_movies_from_genre} genre are:\n' )
     for movie in movies[display_movies_from_genre]:
         print(f'\n - {movie['title']}, \n   rating: {movie['rating']}  \n   Suitable for ages: {movie['age_recommendation']}')
+    
+   
+
 
 #Function to ask about search type
 def start_search():
     search_type = str(input("Would you like to search by name or genre? Write 'n' for 'name' and 'g' for 'genre': "))
-    if search_type == 'g':
+    if search_type.lower() == 'g':
         print(insert_genres())
-    if search_type == 'n':
+    if search_type.lower() == 'n':
         neame_search = input ('Please enter the name of first letter/s of the movie you are looking for: ')
+        #TO DO
 
 #Apply filters function
 def filters(filtered_item):

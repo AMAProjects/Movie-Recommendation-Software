83df885fe3534834e84ceb891b172b1d38952cd1
AMAProjects <angelamolinaalhambra@gmail.com>
2025-02-25 22:54:46 +0000

    Code fixes, name search now working effectively

--- script.py
+++ script.py
@@ -44,9 +44,9 @@ def start_search():
     if search_type.lower() == 'g':
         insert_genres()
     elif search_type.lower() == 'n':
-        print("Name search functionality not implemented yet.")
-        #name_search = input ('\nPlease enter the name of first letter/s of the movie you are looking for: \n')
-        #TO DO
+        #print("\nName search functionality not implemented yet.\n")
+        to_search = input('\nPlease enter the movie name or the first few letters to search:  \n').lower()
+        movie_name_search(movies, to_search)
     else:
         print("\nPlease select either 'g' or 'n'\n")
         start_search()
@@ -54,25 +54,24 @@ def start_search():
 
 
 def movie_name_search(movies_list, to_search):
-    to_search = input('\nPlease enter the movie name or the first few letters to search: ').lower()
     
     matching_names = []
 
-    for genre in movies:
-        for movie in genre:
+    for genre in movies_list:
+        for movie in movies_list[genre]:
             if to_search in movie['title'].lower():
                 matching_names.append(movie)
+            
 
-    print(f"\nSearching for titles that match '{to_search}'...\n\nResults:\n\n")
+    print(f"\nSearching for titles that match '{to_search}'...\n")
  
-    for item in matching_names:
-        print(f'\n - {movie['title']}, \n   Rating: {movie['rating']}  \n   Suitable for ages: {movie['age_recommendation']}')
-        
-
-
-    return movie_name_search(movies, to_search)
-
-
+    
+    if matching_names:
+        print(f"\nFound {len(matching_names)} movie(s) that match '{to_search}':\n")
+        for movie in matching_names:
+            print(f'\n - {movie['title']}, \n   Rating: {movie['rating']}  \n   Suitable for ages: {movie['age_recommendation']}\n')
+    else:
+        print(f"No movies found matching '{to_search}'.")
 
 call_program()
 
